2f4fb166ec91a81d7529ead5b3f05ec3d9931d2f
Ivan Osuna <ivan.osuna.ayuste@gmail.com>
2024-06-26 17:25:19 +0200

    processor

--- src/main/java/com/curso/jobs/job1/steps/step1/Step1.java
+++ src/main/java/com/curso/jobs/job1/steps/step1/Step1.java
@@ -1,4 +1,6 @@
 package com.curso.jobs.job1.steps.step1;
 
-public class Step1 implements IStep1{
+public class Step1 implements IStep1 {
+    // Aqui es donde diremos que queremos usar como procesador para este step un IProcesadorStep1
+    // Spring dará (buscará) la implementación que encuentre
 }
